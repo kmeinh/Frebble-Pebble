cac5df4f7bb7b6c09bc37d986d96e7a77cf281a9
konDeichmann <k.deichmann@mac.com>
2015-07-22 15:26:03 +0200

    a

---
+++ src/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Frebble-Pebble
+//
+//  Created by Konstantin Deichmann on 22.07.15.
+//  Copyright (c) 2015 com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
