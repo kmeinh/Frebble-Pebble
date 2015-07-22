9b6a902f93e7b22681092a38eed23f7b436ad8f1
konDeichmann <k.deichmann@mac.com>
2015-07-22 15:35:55 +0200

    add SoapStuff

--- src/soap/SoapManager.c
+++ src/soap/SoapManager.c
@@ -1,14 +1,9 @@
 //
-//  main.c
+//  SoapManager.c
 //  Frebble-Pebble
 //
 //  Created by Konstantin Deichmann on 22.07.15.
 //  Copyright (c) 2015 com. All rights reserved.
 //
 
-#include <stdio.h>
-#include "soap/SoapManager.h"
-
-int main(int argc, const char * argv[]) {
-    
-}
+#include "SoapManager.h"
---
+++ src/soap/SoapManager.h
@@ -0,0 +1,14 @@
+//
+//  SoapManager.h
+//  Frebble-Pebble
+//
+//  Created by Konstantin Deichmann on 22.07.15.
+//  Copyright (c) 2015 com. All rights reserved.
+//
+
+#ifndef __Frebble_Pebble__SoapManager__
+#define __Frebble_Pebble__SoapManager__
+
+#include <stdio.h>
+
+#endif /* defined(__Frebble_Pebble__SoapManager__) */
